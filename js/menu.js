cc0836e0bf570d698534d6c92de8b48137e0b14e
Ivonne <ivonnebenites2015@gmail.com>
2023-07-25 22:26:35 -0500

    Update: modify error found by ESLint tool

--- js/menu.js
+++ js/menu.js
@@ -5,22 +5,18 @@ const portfolioLink = document.getElementById('portfolio-section');
 const aboutLink = document.getElementById('about-section');
 const contactLink = document.getElementById('contact-section');
 
-const portfolioSection = document.getElementById('portfolio-part');
-
-function appear(){
-    mobileMenuBlue.style.display = 'block';
-    // document.body.style.overflow = 'hidden';
+function appear() {
+  mobileMenuBlue.style.display = 'block';
+  // document.body.style.overflow = 'hidden';
 }
 
-function disappear(){
-    mobileMenuBlue.style.display = 'none';
-    document.body.style.overflow = 'auto';
+function disappear() {
+  mobileMenuBlue.style.display = 'none';
+  document.body.style.overflow = 'auto';
 }
-
 
 closeBtn.addEventListener('click', disappear);
 hamburgerIcon.addEventListener('click', appear);
 portfolioLink.addEventListener('click', disappear);
-aboutLink.addEventListener('click',disappear);
+aboutLink.addEventListener('click', disappear);
 contactLink.addEventListener('click', disappear);
-
