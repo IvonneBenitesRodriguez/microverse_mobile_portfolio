ffee0427f118f4f08f6395de282b568f424ec1a1
Ivonne <ivonnebenites2015@gmail.com>
2023-07-29 11:27:52 -0500

    Chore: add validation functionality to form using JavaScript

--- js/menu.js
+++ js/menu.js
@@ -186,3 +186,16 @@ for (let i = 0; i < projectsData.length; i += 1) {
     });
   });
 }
+
+const email = document.querySelector('#email');
+const validationMessageAlert = document.querySelector('.alert');
+const contactForm = document.querySelector('.contact-form');
+contactForm.addEventListener('submit',(event)=>{
+  const regexInfo = /[A-Z]/;
+  const emailContent = email.value;
+  if(regexInfo.test(emailContent)){
+    validationMessageAlert.innerHTML = 'Your email address should not contain uppercase letters';
+  event.preventDefault();
+  }
+});
+
